60d53b72adaaa626c35b5e1705308d61c76b1761
Adonias Vitorio <adonias.vitorio@gmail.com>
2021-02-07 16:06:33 -0300

    create search Input

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
 
@@ -9,46 +10,61 @@ function Main({ navigation }) {
 
     useEffect(() => {
         async function loadInitialPosition() {
-           const { granted } = await requestPermissionsAsync();
+            const { granted } = await requestPermissionsAsync();
 
-           if(granted) {
-               const { coords } = await getCurrentPositionAsync({
-                   enableHighAccuracy: true,
-               });
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
 
-               const { latitude, longitude } = coords;
+                const { latitude, longitude } = coords;
 
-               setCurrentRegion({
-                   latitude,
-                   longitude,
-                   latitudeDelta: 0.04,
-                   longitudeDelta: 0.04, 
-               })
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
 
-           }
+            }
         }
         loadInitialPosition();
     }, [])
 
-    if(!currentRegion) {
+    if (!currentRegion) {
         return null;
     }
 
     return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-        <Marker coordinate={{ latitude: -23.2029301, longitude: -46.8126656 }}>
-            <Image style={styles.avatar} source={{ uri: 'https://avatars.githubusercontent.com/u/46370687?v=4' }} />
-            <Callout onPress={() => {
-                navigation.navigate('Profile', { github_username: 'adoniasvitorio' });
-            }} >
-                <View style={styles.callout}>
-                    <Text style={styles.devName} >Adonias Vitorio</Text>
-                    <Text style={styles.devBio} >Fullstack Developer, passionate about music and tech.</Text>
-                    <Text style={styles.devTechs} >NodeJS, ReactJS, React Native</Text>
-                </View>
-            </Callout>
-        </Marker>
-    </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -23.2029301, longitude: -46.8126656 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars.githubusercontent.com/u/46370687?v=4' }} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'adoniasvitorio' });
+                    }} >
+                        <View style={styles.callout}>
+                            <Text style={styles.devName} >Adonias Vitorio</Text>
+                            <Text style={styles.devBio} >Fullstack Developer, passionate about music and tech.</Text>
+                            <Text style={styles.devTechs} >NodeJS, ReactJS, React Native</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder='Buscar por Tecnologias'
+                    placeholderTextColor='#999'
+                    autoCapitalize='words'
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                   <MaterialIcons name='my-location' size={20} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -81,6 +97,43 @@ const styles = StyleSheet.create({
 
     devtechs: {
         marginTop: 5,
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+        elevation: 2
     }
 })
 
