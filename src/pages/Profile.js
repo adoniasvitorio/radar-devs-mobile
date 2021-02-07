95175ed0d5ff40457e00666c084794f916dce059
Adonias Vitorio <adonias.vitorio@gmail.com>
2021-02-07 14:08:21 -0300

    create map view to github profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-function Porfile() {
-    return <View />
+function Porfile({navigation}) {
+    const github_username = navigation.getParam('github_username');
+    return <WebView  style={{ flex: 1 }} source={{ uri: `https://github.com/${github_username}` }} />
 }
 
 export default Porfile;
